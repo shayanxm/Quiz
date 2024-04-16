e282148c10c9e11e854d880dee4535d07ab0aa3e
shayan <shayanemoradi@gmail.com>
2024-04-16 18:20:31 +0330

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        //inputs :
+        // what operation -> acc + - * /
+        //learing level 1,2,3
+        //how many questions to awnser -> this should be our while
+        //then generate 2 random numbers and show it
+        //input a resulalt from user
+        // and calculate res base on opreation and compare to users
+        //show true or false show correct awnser
+        //continute
+        //at the end show mean of correct awnsers
+        //show questions and awnsers as resulalt
+    }
+}
